f986626c1ec33dbe9e73d1d0a6bb8e6868aa9977
iaroslavklopov <iaroslavklopov@vk.com>
2022-04-02 11:04:46 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/SongsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Models
+{
+    public class SongsContext : DbContext
+    {
+        public DbSet<Song> Song { get; set; }
+        public SongsContext(DbContextOptions<SongsContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
